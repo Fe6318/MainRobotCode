558f94975651affd4334b2cffeb2ae03f27624c7
Hayden Mumm <haydenmumm@gmail.com>
2018-03-06 23:24:13 -0600

    Tried experimenting with creating an autonomous command, didn't have enough time to finish, so commemented out

---
+++ Getting Started/src/org/usfirst/frc/team6318/robot/LeftStartCommand.java
@@ -0,0 +1,33 @@
+//package org.usfirst.frc.team6318.robot;
+
+//import edu.wpi.first.wpilibj.DriverStation;
+//import edu.wpi.first.wpilibj.command.Command;;
+
+//public class LeftStartCommand extends Command{
+	
+	//private boolean finished;
+	//private Robot;
+	//public LeftStartCommand(Robot robot) {
+	//	Robot = robot
+	//}
+	//
+	//protected void initialize() {
+	//	finished = false;
+	//}
+	
+	//protected void execute() {
+	//	double angle = Robot.m_Gyro.getAngle();
+	//	String gameData;
+	//	gameData = DriverStation.getInstance().getGameSpecificMessage();
+		
+		
+	//}
+	
+	//protected void end() {
+	//	
+	//}
+
+	//protected boolean isFinished() {
+	//	return false;
+	//}
+//}
